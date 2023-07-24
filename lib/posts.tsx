deea19cfbfff03bfb64ae7e757d5f258afc14a99
Niall Moore <niallam22@gmail.com>
2023-07-24 21:45:30 +0100

    correct camel casing

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -4,7 +4,7 @@ import matter from 'gray-matter'
 import { remark } from 'remark'
 import html from 'remark-html'
 
-const postsDirectory = path.join(process.cwd(), 'blogposts')
+const postsDirectory = path.join(process.cwd(), 'blogPosts')
 
 export function getSortedPostsData() {
     // Get file names under /posts
